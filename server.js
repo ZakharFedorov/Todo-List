86709bc56bdbb3528e103aa8ec4773f3cce3a4af
Zakhar_Fedorov <zakhar.fedorov@tul.cz>
2023-01-04 18:58:02 +0100

    first commit

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3333;
 
 app.use(express.static(path.join(__dirname, './')))
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}index.html`);
 });
 
-app.listen(3333, () => {
+app.listen(port, () => {
     console.log('Application listening on port 3333!');
 });
